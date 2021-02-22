81d13416e43bb00f0eb78c68fe6a2e89b5a4945e
Elijah Curme <elijahcurme@elijahs-air.lan>
2021-02-22 14:12:45 -0500

    resolving flask+react

--- team3_webapp_react/src/components/Section2.js
+++ team3_webapp_react/src/components/Section2.js
@@ -3,7 +3,24 @@ import Container from 'react-bootstrap/Container'
 
 const Section2 = () => {
   return(
-    <div className="section3">
+    <div className="section2">
+    <h4>Products</h4>
+    <form class="form" action="/index" method="get">
+    <div class="dropdown">
+    <label for="sort">Sort Products:</label>
+      <select name="sort" id="sort">
+        <option value="recent">Recently Added</option>
+        <option value="top">Top Rated</option>
+      </select>
+    <button class="button" type="submit" onclick="index()">Submit</button>
+    </div>
+    </form>
+<ul>
+  {% for item in inventory %}
+    <li><a href="product.html?product={{item.title}}" id="a">{{item.title}}</a></li>
+    <li>${{item.price}}</li>
+  {% endfor %}
+</ul>
     </div>
   )
 }
