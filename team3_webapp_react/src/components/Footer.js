062d1e0114f9fc3972230f1ca85ac48353f448b0
CHFengbu <fengche2@bu.edu>
2021-02-27 22:04:02 -0500

    Footer Update 2.0

--- team3_webapp_react/src/components/Footer.js
+++ team3_webapp_react/src/components/Footer.js
@@ -6,26 +6,46 @@ import { SocialIcon } from 'react-social-icons';
 
 const Footerpage = () => {
   return (
-    <footer class="text-center text-white" className="footer-container" style="background-color: #363b3f">
-      <div className="container">
-        <section class=" ">
-          <p class="d-flex justify-content-center align-items-center">
-            <span class="me-3" style="color:rgb(255,255,255);font-size:160%;font-family:DejaVu Sans Mono">
-               Register for free
-            </span>
-            <button className="button" style="border: 2px solid grey;border-radius:10%;background-color:#363b3f;color:white;font-size:100%;font-family:DejaVu Sans Mono">
-                Sign Up!
-            </button>
-            <button className="button" style="border: 2px solid grey;border-radius:10%;background-color:#363b3f;color:white;font-size:100%;font-family:DejaVu Sans Mono">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    </head>
+    <footer class="text-center text-white" className="footer-container" style="background-color: #363b3f; padding:100px 80px; width: 1024px">
+       <div className="container">
+         <div class="row">
+            <div class="col-sm-2 col-md-3 item">
+              <img src="./zigzaglogo_only.jpg" alt="zig zag logo" width="100" height="100">
+              <button className="button" style="border: 2px solid grey;border-radius:10%;background-color:#363b3f;color:white;font-size:100%;font-family:DejaVu Sans Mono">
                 Contact Us!
-            </button>
-          </p>
-        </section>
-      </div>
+              </button>
+            </div>
+            <div class="col-sm-2 col-md-3 item">
+              <div class="vl" style="border-left: 1px solid white;height: 160px"></div>
+            </div>
 
-      <small>
-        <div className="footer-copyright text-center py-3" style="color:white">&copy; {new Date().getFullYear()} Copyright: <a href='#' style="color:orange"> ziczac.com </a></div>
-      </small>
+            <div class="col-sm-2 col-md-3 item">
+              <span class="me-3" style="color:rgb(255,255,255);font-size:150%;font-family:DejaVu Sans Mono">
+                Register for free
+              </span>
+              <br><br>
+              <button className="button" style="border: 2px solid grey;border-radius:10%;background-color:#363b3f;color:white;font-size:100%;font-family:DejaVu Sans Mono">
+                Sign Up!
+              </button>
+              <br><br>
+              <small style="position:static">
+                  <div className="footer-copyright text-center py-3" style="color:gold">&copy;Copyright
+                    <span style="color:white">:</span>
+                    <a href='#' style="color:orange">
+                      ziczac.com
+                    </a>
+                  </div>
+               </small>
+           </div>
+         </div>
+
+       </div>
+
      </footer>
   );
 }
