efbe27d0801b338a6732cbb56cc81241fb635cd1
Elijah Curme <elijahcurme@elijahs-air.lan>
2021-03-05 18:36:28 -0500

    fixed price sort

--- dev/project_classes.py
+++ dev/project_classes.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 import json
 
-class Item:    
+class Item:
     def __init__(self, title, price):
         self.title = title
         self.price = price
@@ -12,57 +12,60 @@ class Item:
         self.dateAdded = arrow.now().format('YYYY-MM-DD')
         self.ratings = []
         self.tags = []
-        
+
     def add_description(self, desc):
         self.desc = desc
-        
+
     def add_tag(self, tag):
         self.tags.append(tag)
-    
+
     def add_seller(self, seller):
         self.seller = seller
-    
+
     def rate(self, rating):
         self.ratings.append(rating)
-        
+
     def get_rating(self):
         return np.mean(self.ratings)
-    
+
     def get_date(self):
         return self.dateAdded
-    
+
     def get_tags(self):
         return self.tags
-    
+
+    def get_price(self):
+        return self.price
+
     def to_JSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=False, indent=4)
-        
+
 class Order:
     def __init__(self):
         self.orderDate = arrow.now().format('YYYY-MM-DD')
         self.total = 0
         self.items = []
-        
+
     def add_item(self, item):
         self.items.append(item)
         self.total += item.price
-        
+
     def get_items(self):
         return self.items
-    
+
 class Account:
     def __init__(self, password, email):
         #self.username = username
         self.password = password
         self.email = email
         self.orders = []
-        
+
     def add_email(self, email):
         self.email = email
-        
+
     def add_order(self, order):
         self.orders.append(order)
-        
+
     def get_orders(self):
         return self.orders
 
