39a5b68ca3b5873c876c3035a57e9ee7f45496d1
Pelin Akbiyik <pakbiyik@bu.edu>
2021-03-22 22:08:41 -0400

    Update test_homepage.py

--- dev/automated_tests/chrome_tests/test_homepage.py
+++ dev/automated_tests/chrome_tests/test_homepage.py
@@ -10,9 +10,12 @@ class homepagetest(unittest.TestCase):
 
 
       def test_Homepageloads(self):
-#checking to see if account botton works by checking sign up button in acount page
+#checking to see if account botton works by checking sign up element in acount page
           self.driver.find_element_by_xpath('//*[@id="root"]/div/div/nav/div[2]/a[2]').click()
           expr=self.driver.find_element_by_xpath('//*[@id="root"]/div/div/div/ul/li[2]/a').text
           print(expr)
           self.assertEqual(expr, 'Sign Up')
+          time.sleep(1)
 
+      def teardown(self):
+          self.driver.close()
