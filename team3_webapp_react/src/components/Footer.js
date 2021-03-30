2a4f7c7ff6d7f9735cf8e24212b4c8f2f8936767
CHFengbu <fengche2@bu.edu>
2021-03-30 04:01:55 -0400

    About Us Link
    If anyone who want to connect the footer to AboutUs page, just modify link 25 to that link.

--- team3_webapp_react/src/components/Footer.js
+++ team3_webapp_react/src/components/Footer.js
@@ -22,7 +22,9 @@ const Footerpage = () => {
         <div classname="container">
           <div className="row">
             <div className="col-sm-2 col-md-3 item">
+              <Link to="../Home">
               <img src={logo} width="100" height="100" />
+              </Link>
               <br />
               <br />
               <Link to="../ContactUs">
@@ -40,6 +42,7 @@ const Footerpage = () => {
                 Contact Us!
               </button>
               </Link>
+
             </div>
             <div className="col-sm-2 col-md-3 item">
               <div
@@ -80,8 +83,10 @@ const Footerpage = () => {
 
               </button>
               </Link>
+
               <br />
               <br />
+
               <small
                 style={{
                   position: "static"
@@ -111,6 +116,7 @@ const Footerpage = () => {
                   </a>
                 </div>
               </small>
+
             </div>
           </div>
         </div>
