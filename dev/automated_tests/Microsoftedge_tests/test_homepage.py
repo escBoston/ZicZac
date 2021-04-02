dfd6b9748aeabc0a8afe388b5001cdb7a28f7cb3
Pelin Akbiyik <pakbiyik@bu.edu>
2021-04-02 19:12:06 -0400

    trying to set up selenium

--- dev/automated_tests/Microsoftedge_tests/test_homepage.py
+++ dev/automated_tests/Microsoftedge_tests/test_homepage.py
@@ -1,21 +1,34 @@
+import os
 import unittest
+from msedge.selenium_tools import Edge, EdgeOptions
 from selenium import webdriver
-from webdriver_manager.microsoft import EdgeDriverManager
+from selenium.webdriver.common.keys import Keys
+#from webdriver_manager.microsoft import EdgeDriverManager
 
 
-driver = webdriver.Edge(executable_path=EdgeDriverManager().install())
-driver.get("http://www.google.com/")
-print(driver.title)
-driver.quit()
+#driver = webdriver.Edge(executable_path=EdgeDriverManager().install())
+##driver.get("http://www.google.com/")
+#print(driver.title)
+#driver.quit()
 #import time
 
 
-#class homepagetest(unittest.TestCase):
+class homepagetest(unittest.TestCase):
 
-      #def setUp(self):
+      def setUp(self):
+        #options = EdgeOptions()
+        #options.use_chromium = True
+        #options.binary_location = "C:\\Users\\pakbi\\AppData\\Local\\Programs\\Python\\Python39\\msedgedriver.exe"
+        self.driver = webdriver.Chrome("C:/Users/pakbi/Documents/GitHub/BUMETCS673S21T3/dev/automated_tests/Microsoftedge_tests/Drivers/chromedriver.exe")
+        #dir = os.path.dirname(os.path.realpath(__file__))
+       # edge_driver_path = dir+"\\edgedriver_win64\\msedgedriver.exe"
+       # self.driver = Edge(options=options, executable_path=edge_driver_path)
+        self.driver.implicity_wait(30)
+        self.driver.maximize_window()
+        self.driver.get("www.facebook.com")
           #self.driver = webdriver.edge()
           #self.driver = webdriver.edge(executable_path="C:\\Users\\pakbi\\Documents\\GitHub\\BUMETCS673S21T3\\dev\\automated_tests\\Microsoftedge_tests\\msedgedriver")
-          #self.driver.get("http://localhost:3000/Home")
+          #self.driver.get("http://localhost:3000/")
             
           
 
