1e50260d278dd90ddd4cc8c195602a33852a198b
Pelin Akbiyik <pakbiyik@bu.edu>
2021-04-06 21:48:52 -0400

    Updates

--- automated_tests/test_signuppage.py
+++ automated_tests/test_signuppage.py
@@ -2,6 +2,9 @@ import os
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import time
 
 
@@ -17,16 +20,19 @@ class signuppagefunctionality(unittest.TestCase):
 
     def test_signup(self):
 
-    #checking to see if users can signup
+    # checking to see if users can signup
 
-        email = self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[1]/input')
+        email = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[1]/input')
         email.send_keys("testingproject@gmail.com")
-        username = self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[2]/input')
+        username = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[2]/input')
         username.send_keys("Jacob15")
-        password=self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[3]/input')
+        password = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[3]/input')
         password.send_keys("Beauty1234@")
         self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/button').click()
-        time.sleep(3)
+        time.sleep(5)
         exps = self.driver.find_element_by_xpath('//*[@id="root"]/div/div/div[2]/div/div/h1').text
         print(exps)
         self.assertEqual(exps, "PRODUCTS")
@@ -34,13 +40,19 @@ class signuppagefunctionality(unittest.TestCase):
 
     def test_errormessage(self):
 
-        #checking to see if the error message displayed when user does not meet the password requirements
+        # checking to see if the error message displayed when user does not meet the password requirements
+        email = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[1]/input')
         email.send_keys("testingproject@gmail.com")
-        username = self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[2]/input')
+        username = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[2]/input')
         username.send_keys("Jacob15")
-        password=self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[3]/input')
+        password = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[3]/input')
         password.send_keys("abcd")
-        self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/button').click()
+        self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/button').click()
+        time.sleep(1)
         exps1 = self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/span').text
         print(exps1)
         self.assertEqual(exps1, "password requirements not met!")
@@ -48,24 +60,136 @@ class signuppagefunctionality(unittest.TestCase):
 
     def test_existinguser(self):
 
-        #checking to see if the error message displayed when user enters existing user info
+        # checking to see if the error message displayed when user enters existing user info
+        email = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[1]/input')
         email.send_keys("testingproject@gmail.com")
-        username = self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[2]/input')
+        username = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[2]/input')
         username.send_keys("securitytest")
-        password=self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[3]/input')
+        password = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[3]/input')
         password.send_keys("Securitytest1!")
-        self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/button').click()
-        exps2 = self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/span').text
+        self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/button').click()
+        time.sleep(1)
+        exps2 = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/span').text
         print(exps2)
         self.assertEqual(exps2, "username taken")
         time.sleep(1)
 
-    def test_blankfields(self):
-        #Checking the error message when user does not enter empty fields.
-        email= self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/button')
+    def test_blankemail(self):
+        # Checking the error message when user does not enter email field.
+        email = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[1]/input')
+        email.send_keys(" ")
+        username = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[2]/input')
+        username.send_keys("test590")
+        password = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[3]/input')
+        password.send_keys("Boston1234@")
+        self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/button').click()
+        wait = WebDriverWait(self.driver, 2)  
+        try:
+            wait.until(EC.alert_is_present(), 'Alert did not show')
+        
+
+            alert = self.driver.switch_to.alert
+            alert.accept()
+            print("alert accepted")
+        except TimeoutException:
+            print("no alert")
 
 
- def teardown(self):
+        
+
+         # how to inspect an alert?
+
+    def test_emptyusername(self):
+    # Checking the error message when user does not enter username field
+        email = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[1]/input')
+        email.send_keys("test2021@gmail.com")
+        username = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[2]/input')
+        username.send_keys(" ")
+        password = self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/div[3]/input')
+        password.send_keys("Boston123456@")
+        self.driver.find_element_by_xpath(
+            '//*[@id="root"]/div/div/form/button').click()
+        wait = WebDriverWait(self.driver, 2)  
+        try:
+            wait.until(EC.alert_is_present(), 'Alert did not show')
+        
+
+            alert = self.driver.switch_to.alert
+            alert.accept()
+            print("alert accepted")
+        except TimeoutException:
+            print("no alert")
+
+
+      
+
+         # how to inspect an alert?
+    def test_passwordfieldempty(self):
+        # Checking the error message when user does not enter password fields
+        email= self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[1]/input')
+        email.send_keys("test9010gmail.com")
+        username= self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[2]/input')
+        username.send_keys("Nicolas90")
+        password= self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[3]/input')
+        password.send_keys(" ")
+        self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/button').click()
+        wait = WebDriverWait(self.driver, 2)  
+        try:
+            wait.until(EC.alert_is_present(), 'Alert did not show')
+        
+
+            alert = self.driver.switch_to.alert
+            alert.accept()
+            print("alert accepted")
+        except TimeoutException:
+            print("no alert")
+
+
+
+
+          # how to inspect an alert?
+
+    def test_allfieldsempty(self):
+       # Checking the error message when user does not enter any fields
+        email= self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[1]/input')
+        email.send_keys(" ")
+        username= self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[2]/input')
+        username.send_keys(" ")
+        password= self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/div[3]/input')
+        password.send_keys(" ")
+        self.driver.find_element_by_xpath('//*[@id="root"]/div/div/form/button').click()
+        wait = WebDriverWait(self.driver, 2)  
+        try:
+            wait.until(EC.alert_is_present(), 'Alert did not show')
+        
+
+            alert = self.driver.switch_to.alert
+            alert.accept()
+            print("alert accepted")
+        except TimeoutException:
+            print("no alert")
+
+
+
+        # how to inspect an alert?
+
+
+        
+        
+
+    def teardown(self):
         self.driver.close()
 
 
